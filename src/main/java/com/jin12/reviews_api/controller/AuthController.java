2377d5257cbdc5a449781bc4ddff7b9fa8987c3e
david-andreasson <79davand@gafe.molndal.se>
2025-04-11 14:07:09 +0200

    Feat created register and login feature with JWT token example-code from previous lessons

---
+++ src/main/java/com/jin12/reviews_api/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.jin12.reviews_api.controller;
+
+import com.jin12.reviews_api.dto.AuthenticationRequest;
+import com.jin12.reviews_api.dto.AuthenticationResponse;
+import com.jin12.reviews_api.dto.RegisterRequest;
+import com.jin12.reviews_api.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        AuthenticationResponse response = authenticationService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        AuthenticationResponse response = authenticationService.authenticate(request);
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/com/jin12/reviews_api/model/User.java
+++ src/main/java/com/jin12/reviews_api/model/User.java
@@ -1,21 +1,57 @@
 package com.jin12.reviews_api.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Entity
-@Data
 @Table(name = "users")
-public class User {
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
+
+    @Column(nullable = false)
+    private String password;
 
     @OneToMany
     private List<Product> products;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
