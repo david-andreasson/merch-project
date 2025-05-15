51d4f93fdfafa43c8bf7c998716b8a84c6c7eb11
Erik <erik.thorsell.95@gmail.com>
2025-05-15 09:48:12 +0200

    Ändrade ordning pom-filen och implementerade Jacoco

--- src/test/java/com/jin12/reviews_api/repository/UserRepositoryTest.java
+++ src/test/java/com/jin12/reviews_api/repository/UserRepositoryTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @DataJpaTest
 class UserRepositoryTest {
 
